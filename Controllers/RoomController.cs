92793f0326f882174374ccb64254495e0e1795a5
pntrgergo <pntrgergo@gmail.com>
2023-04-26 19:19:39 +0200

    Feat: Room controller and methods

--- Controllers/RoomController.cs
+++ Controllers/RoomController.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using HogwartsPotions.Models;
+using HogwartsPotions.Data;
 using HogwartsPotions.Models.Entities;
+using HogwartsPotions.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HogwartsPotions.Controllers
@@ -9,47 +10,47 @@ namespace HogwartsPotions.Controllers
     [ApiController, Route("/room")]
     public class RoomController : ControllerBase
     {
-        private readonly HogwartsContext _context;
+        private readonly IRoomService _roomService;
 
-        public RoomController(HogwartsContext context)
+        public RoomController(IRoomService roomService)
         {
-            _context = context;
+            _roomService = roomService;
         }
 
         [HttpGet]
         public async Task<List<Room>> GetAllRooms()
         {
-            return await _context.GetAllRooms();
+            return await _roomService.GetAllRooms();
         }
 
         [HttpPost]
         public async  Task AddRoom([FromBody] Room room)
         {
-            await _context.AddRoom(room);
+            await _roomService.AddRoom(room);
         }
 
         [HttpGet("/{id}")]
         public async Task<Room> GetRoomById(long id)
         {
-            return await _context.GetRoom(id);
+            return await _roomService.GetRoomById(id);
         }
 
         [HttpPut("/{id}")]
         public void UpdateRoomById(long id, [FromBody] Room updatedRoom)
         {
-            _context.Update(updatedRoom);
+            _roomService.UpdateRoomById(updatedRoom);
         }
 
         [HttpDelete("/{id}")]
-        public async Task DeleteRoomById(long id)
+        public async Task DeleteRoomById(Room room)
         {
-            await _context.DeleteRoom(id);
+            await _roomService.DeleteRoom(room);
         }
 
         [HttpGet("/rat-owners")]
         public async Task<List<Room>> GetRoomsForRatOwners()
         {
-            return await _context.GetRoomsForRatOwners();
+            return await _roomService.GetRoomsForRatOwners();
         }
     }
 }
