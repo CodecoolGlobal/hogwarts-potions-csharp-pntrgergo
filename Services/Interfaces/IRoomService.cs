3f730013c40ae9d2b215be214865ff004ab9ec4d
pntrgergo <pntrgergo@gmail.com>
2023-04-26 19:15:09 +0200

    Feat: RoomService

---
+++ Services/Interfaces/IRoomService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HogwartsPotions.Models.Entities;
+
+namespace HogwartsPotions.Services.Interfaces;
+
+public interface IRoomService
+{
+
+    Task AddRoomToDb(Room room);
+    Task<List<Room>> GetAllRooms();
+    Task<Room> GetRoomById(long roomId);
+    Task<List<Room>> GetRoomsForRatOwners();
+    Task UpdateRoomById(Room room);
+    Task DeleteRoom(Room room);
+}
---
+++ Services/RoomService.cs
@@ -0,0 +1,40 @@
+﻿using HogwartsPotions.Models.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using HogwartsPotions.Services.Interfaces;
+
+namespace HogwartsPotions.Services
+{
+    public class RoomService : IRoomService
+    {
+        public Task AddRoom(Room room)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteRoom(Room room)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<Room>> GetAllRooms()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Room> GetRoom(long roomId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<Room>> GetRoomsForRatOwners()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task UpdateRoom(Room room)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
